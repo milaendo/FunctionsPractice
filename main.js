b7c4c29129210dc5b15b588e7c08fc0377bbd1b8
Mila Endo <milal.alim@gmail.com>
2017-07-06 14:36:18 -0700

    fixed missing parenthesese

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function maxOfThree(num1, num2, num3){
     	return 'num3'
     }
 }
-console.log (maxOfThree(2,3,4)
+console.log (maxOfThree(2,3,4))
 
 // 3.
 // Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
